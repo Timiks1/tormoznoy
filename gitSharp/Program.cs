f6c52d929fb6cc723474f799d436c57ef45a9030
Timiks1 <prozor2@ukr.net>
2022-11-09 11:07:53 +0200

    first commit

--- gitSharp/Program.cs
+++ gitSharp/Program.cs
@@ -3,19 +3,24 @@ int type = int.Parse(Console.ReadLine());
 Console.WriteLine("введите вашу скорость в км/ч");
 int speed = int.Parse(Console.ReadLine());
 
-double trueType = 0.7;
-if(type == 1)
+
+
+double calculate(int type,int speed)
 {
-    trueType = 0.7;
-}
-else if(type == 2)
-{
-    trueType = 0.4;
-}
-else if (type == 3)
-{
-    trueType = 0.02;
+    double trueType = 0.7;
+    if (type == 1)
+    {
+        trueType = 0.7;
+    }
+    else if (type == 2)
+    {
+        trueType = 0.4;
+    }
+    else if (type == 3)
+    {
+        trueType = 0.02;
+    }
+    return (speed * speed) / (254 * trueType);
 }
 
-double pass = (speed * speed) / (254 * trueType);
-Console.WriteLine("ваш тормозной путь = "+pass + " метров");
+Console.WriteLine("ваш тормозной путь = "+calculate(type,speed) + " метров");
